873476d9554217d18f14960948d8815b2345fafc
cizcou <guilhermecsti01@gmail.com>
2022-01-10 08:09:35 -0300

    Primeriro commit

--- App.js
+++ App.js
@@ -1,163 +1,273 @@
-import React, { useState } from 'react';
-import { View, Text, StyleSheet, SafeAreaView, StatusBar, 
-  TouchableOpacity, Modal, TextInput, Image, FlatList } from 'react-native';
-import { Ionicons } from '@expo/vector-icons';
+import React, { useState } from 'react'
+import {
+  View,
+  Text,
+  StyleSheet,
+  SafeAreaView,
+  StatusBar,
+  TouchableOpacity,
+  Modal,
+  ScrollView,
+  TextInput,
+  Image
+} from 'react-native'
+import { Ionicons } from '@expo/vector-icons'
 
+import * as Animatable from 'react-native-animatable'
+import { FaSpider } from 'react-icons/fa'
 
-import * as Animatable from 'react-native-animatable';
-
-const AnimatedBtn = Animatable.createAnimatableComponent(TouchableOpacity);
+const AnimatedBtn = Animatable.createAnimatableComponent(TouchableOpacity)
 
 export default function App() {
+  const [page, setPage] = useState(false)
+  const [input, setInput] = useState('')
+  const [image, setImage] = useState(false)
+  const [link, setLink] = useState('')
+  const [descript, setDescript] = useState('')
+  function verify() {
+    if (input === '') {
+      alert('Não deixe o campo vazio por gentileza')
+      return
+    }
 
-  const [door, setDoor] = useState(false);
-  const [input, setInput] = useState('');
-  const [image, setImage] = useState(false);
-  const [link, setLink] = useState('');
-  const [descript, setDescript] = useState('');
-  
-
-
-  function verify(){
-    if (input === '') {alert('Insira um nome válido!'); return;};
-
-    setDoor(true);
+    setPage(true)
   }
-  function one(){
-    setImage(true);
-    setLink(require('./src/images/prod1.jpg'));
-    setDescript('R$ 199.99 || Adorno em Ouro, pingente Família 4 integrantes');
+  function primeira() {
+    setImage(true)
+    setLink(require('./src/images/prod1.jpg'))
+    setDescript('R$ 249.99 | Roupa Homem aranha Andrew')
   }
-  function two(){
-    setImage(true);
-    setLink(require('./src/images/prod2.jpg'));
-    setDescript('R$ 799.99 ||Adorno em Obsidiana, pingente Yin e Yang');
+  function segunda() {
+    setImage(true)
+    setLink(require('./src/images/prod2.jpg'))
+    setDescript('R$ 249.99 | Roupa Homem aranha Tobey')
   }
-  function three(){
-    setImage(true);
-    setLink(require('./src/images/prod3.jpg'));
-    setDescript('R$ 99.99 ||Adorno em Ouro, pingente de Leopardo');
+  function terceira() {
+    setImage(true)
+    setLink(require('./src/images/prod3.jpg'))
+    setDescript('R$ 249.99 | Roupa aranha de ferro')
   }
-  function four(){
-    setImage(true);
-    setLink(require('./src/images/prod4.jpg'));
-    setDescript('R$ 209.99 ||Adorno em Cobre, pingente Estrela de David');
+  function quarta() {
+    setImage(true)
+    setLink(require('./src/images/prod4.jpg'))
+    setDescript('R$ 249.99 | Roupa aranha Classico')
   }
-  function five(){
-    setImage(true);
-    setLink(require('./src/images/prod5.jpg'));
-    setDescript('R$ 159.99 ||Adorno em Ouro, edição Cavalgada');
+  function quinta() {
+    setImage(true)
+    setLink(require('./src/images/prod5.jpg'))
+    setDescript('R$ 249.99 | Roupa aranha 2001')
   }
-  function liu(){
-    setImage(true);
-    setLink(require('./src/images/prod6.jpg'));
-    setDescript('R$ 499.99 ||Adorno em Ouro, edição Gamer - Pingente Olho da Verdade');
+  function sexta() {
+    setImage(true)
+    setLink(require('./src/images/prod6.jpg'))
+    setDescript('R$ 249.99 | Roupa aranha Jogo ')
   }
   return (
     <SafeAreaView style={styles.container}>
-      <StatusBar backgroundColor='#171d31' barStyle='light-content' />
+      <StatusBar backgroundColor="#171d31" barStyle="light-content" />
 
-      <Animatable.View animation="fadeInUp" useNativeDriver>
-        <Text style={styles.title}>Loja do Antonio Neto</Text>
-        
-        <TextInput 
-        placeholder='Usuário'
-        placeholderTextColor='#747474'
+      <Text style={styles.title}>Loja do Francisco Guilherme</Text>
+
+      <TextInput
+        placeholder="Cliente"
+        placeholderTextColor="#DCDCDC"
         value={input}
         style={styles.input}
-        onChangeText={ (texto) => setInput(texto) }
-        />
+        onChangeText={texto => setInput(texto)}
+      />
 
-          <TouchableOpacity onPress={verify} style={styles.head}>
-            <Ionicons style={{marginLeft: 5, marginRight: 5}}name="md-enter" size={40} color="#DDD" />
-            <Text style={{color: "#DDD"}}>Entrar</Text>
-          </TouchableOpacity>
+      <TouchableOpacity onPress={verify} style={styles.head}>
+        <FaSpider />
+        <Text style={{ color: '#DDD' }}>Logar</Text>
+      </TouchableOpacity>
 
-      </Animatable.View>
-
-      <Modal animationtype='bounceInUp' transparent={false} visible={door} >
+      <Modal animationtype="bounceInUp" transparent={false} visible={page}>
         <SafeAreaView style={styles.container}>
+          <View style={styles.line}>
+            <Text style={styles.title}>Loja do Francisco Guilherme</Text>
 
-            <Text style={styles.title}>
-              Loja do Antonio Neto
-            </Text>
-
-            <Text style={styles.subtitle}> 
-              Bem vindo: {input} 
-            </Text>
-
-          <View style={styles.images}>
-            <TouchableOpacity onPress={one}>
-              <Image  
-                source={require('./src/images/prod1.jpg')} 
-                style={{height: 150, width: 150, flexDirection: 'row'}} 
-              />
-            </TouchableOpacity>
+            <Text style={styles.title}>Bem vindo: {input}</Text>
           </View>
+          <ScrollView>
+            <View
+              style={{
+                flexDirection: 'row',
+                justifyContent: 'space-around',
+                alignItems: 'center',
+                alignContent: 'center'
+              }}
+            >
+              <View style={styles.images}>
+                <TouchableOpacity
+                  onPress={primeira}
+                  style={{
+                    position: 'absolute',
+                    right: 0,
+                    alignSelf: 'center'
+                  }}
+                >
+                  <Image
+                    source={require('./src/images/prod1.jpg')}
+                    style={{
+                      height: 100,
+                      width: 100,
+                      flexDirection: 'row',
+                      justifyContent: 'space-around'
+                    }}
+                  />
+                  <Text style={[styles.text, { color: '#000000' }]}>
+                    Homem aranha 1
+                  </Text>
+                </TouchableOpacity>
+              </View>
 
-          <View style={styles.images}>
-            <TouchableOpacity onPress={two}>
-              <Image 
-                source={require('./src/images/prod2.jpg')} 
-                style={{height: 150, width: 150, flexDirection: 'row'}} 
-              />
-            </TouchableOpacity>
-          </View>
+              <View style={styles.images}>
+                <TouchableOpacity
+                  onPress={segunda}
+                  style={{
+                    position: 'absolute',
+                    right: 0,
+                    alignSelf: 'center'
+                  }}
+                >
+                  <Image
+                    source={require('./src/images/prod2.jpg')}
+                    style={{
+                      height: 100,
+                      width: 100,
+                      flexDirection: 'row',
+                      justifyContent: 'space-around'
+                    }}
+                  />
+                  <Text style={[styles.text, { color: '#000000' }]}>
+                    Homem aranha 2
+                  </Text>
+                </TouchableOpacity>
+              </View>
 
-          <View style={styles.images}>
-            <TouchableOpacity onPress={three}>
-                <Image  
-                    source={require('./src/images/prod3.jpg')} 
-                    style={{height: 150, width: 150, flexDirection: 'row'}} 
-                />
-            </TouchableOpacity>
-          </View>
-          
-          <View style={styles.images}>
-            <TouchableOpacity onPress={four}>
-              <Image  
-                source={require('./src/images/prod4.jpg')} 
-                style={{height: 150, width: 150, flexDirection: 'row'}} 
-              />
-            </TouchableOpacity>
-          </View>
+              <View style={styles.images}>
+                <TouchableOpacity
+                  onPress={terceira}
+                  style={{
+                    position: 'absolute',
+                    right: 0,
+                    alignSelf: 'center'
+                  }}
+                >
+                  <Image
+                    source={require('./src/images/prod3.jpg')}
+                    style={{
+                      height: 100,
+                      width: 100,
+                      flexDirection: 'row',
+                      justifyContent: 'space-around'
+                    }}
+                  />
+                  <Text style={[styles.text, { color: '#000000' }]}>
+                    Homem aranha 3
+                  </Text>
+                </TouchableOpacity>
+              </View>
 
-          <View style={styles.images}>
-            <TouchableOpacity onPress={five}>
-              <Image  
-                source={require('./src/images/prod5.jpg')} 
-                style={{height: 150, width: 150, flexDirection: 'row'}} 
-              />
-            </TouchableOpacity>
-          </View>
+              <View style={styles.images}>
+                <TouchableOpacity
+                  onPress={quarta}
+                  style={{
+                    position: 'absolute',
+                    right: 0,
+                    alignSelf: 'center'
+                  }}
+                >
+                  <Image
+                    source={require('./src/images/prod4.jpg')}
+                    style={{
+                      height: 100,
+                      width: 100,
+                      flexDirection: 'row',
+                      justifyContent: 'space-around'
+                    }}
+                  />
+                  <Text style={[styles.text, { color: '#000000' }]}>
+                    Homem aranha 4
+                  </Text>
+                </TouchableOpacity>
+              </View>
 
-          <View style={styles.images}>
-            <TouchableOpacity onPress={liu}>
-              <Image  
-                source={require('./src/images/prod6.jpg')} 
-                style={{height: 150, width: 150, flexDirection: 'row'}} 
-              />
-            </TouchableOpacity>
-          </View>
+              <View style={styles.images}>
+                <TouchableOpacity
+                  onPress={quinta}
+                  style={{
+                    position: 'absolute',
+                    right: 0,
+                    alignSelf: 'center'
+                  }}
+                >
+                  <Image
+                    source={require('./src/images/prod5.jpg')}
+                    style={{
+                      height: 100,
+                      width: 100,
+                      flexDirection: 'row',
+                      justifyContent: 'space-around'
+                    }}
+                  />
+                  <Text style={[styles.text, { color: '#000000' }]}>
+                    Homem aranha 5
+                  </Text>
+                </TouchableOpacity>
+              </View>
 
+              <View style={styles.images}>
+                <TouchableOpacity
+                  onPress={sexta}
+                  style={{
+                    position: 'absolute',
+                    right: 0,
+                    alignSelf: 'center'
+                  }}
+                >
+                  <Image
+                    source={require('./src/images/prod6.jpg')}
+                    style={{
+                      height: 100,
+                      width: 100,
+                      flexDirection: 'row',
+                      justifyContent: 'space-around'
+                    }}
+                  />
+                  <Text style={[styles.text, { color: '#000000' }]}>
+                    Homem aranha 6
+                  </Text>
+                </TouchableOpacity>
+              </View>
+            </View>
+          </ScrollView>
         </SafeAreaView>
       </Modal>
 
-      <Modal animationtype='bounceInUp' transparent={false} visible={image} style={{backgroundColor: '#000'}}>
-
-          <TouchableOpacity onPress={ () => setImage(false) }>
-            <Ionicons style={{marginLeft: 5, marginRight: 5}}name="md-arrow-back" size={40} color="#DDD" />
-          </TouchableOpacity>
-          <Image source={link} style={styles.into} />
-          <Text style={{textAlign:'center'}}>{descript}</Text>  
-        
+      <Modal
+        animationtype="bounceInUp"
+        transparent={false}
+        visible={image}
+        style={{ backgroundColor: '#000' }}
+      >
+        <TouchableOpacity onPress={() => setImage(false)}>
+          <Ionicons
+            style={{ marginLeft: 5, marginRight: 5 }}
+            name="md-arrow-back"
+            size={40}
+            color="#DDD"
+          />
+        </TouchableOpacity>
+        <Image source={link} style={styles.into} />
+        <Text style={{ textAlign: 'center' }}>{descript}</Text>
       </Modal>
     </SafeAreaView>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
-  images:{
+  images: {
     height: 150,
     width: 150,
     backgroundColor: '#FFF',
@@ -166,12 +276,15 @@ const styles = StyleSheet.create({
     marginRight: 10,
     borderColor: '#000',
     flexWrap: 'wrap',
-    flexDirection: 'row',
+    flexDirection: 'column',
     alignSelf: 'center',
     borderRadius: 7,
-    padding: 2,
+    padding: 2
   },
-  into:{
+  header: {
+    marginBottom: 8
+  },
+  into: {
     height: 460,
     width: 320,
     backgroundColor: '#FFF',
@@ -182,54 +295,76 @@ const styles = StyleSheet.create({
     alignSelf: 'center',
     borderRadius: 7,
     padding: 2,
-    textAlign: 'center',
-
+    textAlign: 'center'
   },
-  input:{
-    fontSize:15,
-    marginLeft:10,
-    marginRight:10,
-    marginTop:30,
-    backgroundColor:'#FFF',
-    padding:9,
+  input: {
+    fontSize: 15,
+    marginLeft: 10,
+    marginRight: 10,
+    marginTop: 30,
+    backgroundColor: '#FFF',
+    padding: 9,
     height: 45,
-    textAlignVertical: 'top',
+    textAlignVertical: 'center',
     color: '#000',
-    borderRadius: 5,
+    borderRadius: 5
   },
-  container:{
-    flex:1,
-    backgroundColor: "#000"
+  image: {
+    width: '100%'
   },
-  title:{
+  container: {
+    flex: 1,
+    width: '100%',
+    backgroundColor: '#A52A2A'
+  },
+  textContainer: {
+    flexDirection: 'row',
+    marginVertical: '5%',
+    marginHorizontal: '5%'
+  },
+  text: {
+    fontSize: 15,
+    flex: 1,
+    marginHorizontal: '1%',
+    textAlign: 'left'
+  },
+  title: {
     marginTop: 10,
     paddingBottom: 10,
     fontSize: 25,
     textAlign: 'center',
     color: '#DDD'
   },
-  subtitle:{
-    marginTop:10,
+  line: {
+    borderBottomColor: '#D8d8d8',
+    borderBottomWidth: 2
+  },
+  subtitle: {
+    marginTop: 10,
     paddingBottom: 10,
     fontSize: 15,
     textAlign: 'center',
-    color: '#DDD',
+    color: '#000'
   },
-  enter:{
+  textContent: {
+    fontSize: 16,
+    lineHeight: 25,
+    marginVertical: '2%',
+    paddingHorizontal: '2%'
+  },
+  enter: {
     color: '#FFF',
     fontSize: 20,
     marginLeft: 15,
     alignItems: 'center',
-    alignSelf: 'center',
+    alignSelf: 'center'
   },
-  head:{
-    marginLeft:10,
+  head: {
+    marginLeft: 10,
     marginTop: 20,
     flexDirection: 'row',
     alignItems: 'center',
     color: '#FFF',
-    alignSelf: 'center',
+    alignSelf: 'center'
   }
 })
-
-
